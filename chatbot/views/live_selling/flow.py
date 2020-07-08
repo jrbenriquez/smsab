feb56a2a7cbb4885e9e9fac14667f874ce613510
John Rei Enriquez <johnrei.enriquez@gmail.com>
2020-07-09 01:25:22 +0800

    add Get to entrypoint

--- chatbot/views/live_selling/flow.py
+++ chatbot/views/live_selling/flow.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from chatbot.models.users import MessengerProfile
 from chatbot.permissions.manychat import ManyChatAppPermission
@@ -13,7 +14,32 @@ class EntryPointViewSet(ModelViewSet):
     queryset = MessengerProfile.objects.all()
     serializer_class = MessengerProfileSerializer
     permission_classes = [ManyChatAppPermission]
-    http_method_names = ['post', 'head']
+    http_method_names = ['post', 'get']
+
+    @action(methods=['get'], detail=True,
+            url_path='greeting', url_name='greeting')
+    def greeting(self, request, pk=None):
+        response_data = {
+            "version": "v2",
+            "content": {
+            "messages": [
+              {
+                "type": "text",
+                "text": "simple text with button",
+                "buttons": [
+                  {
+                    "type": "url",
+                    "caption": "External link",
+                    "url": "https://manychat.com"
+                  }
+                ]
+              }
+            ],
+            "actions": [],
+            "quick_replies": []
+            }
+            }
+        return Response(response_data)
 
     def create(self, request, *args, **kwargs):
         data = request.data
